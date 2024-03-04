af51a6ea675a2d184acab784141922484845de5b
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-04 21:51:45 +0800

    auto create job prepared

--- src/com/seh/utils/autoCreateJob.groovy
+++ src/com/seh/utils/autoCreateJob.groovy
@@ -8,7 +8,7 @@ def call(String repo,String appName) {
       withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'svc_devops_jenkins_token',
       usernameVariable: 'USER', passwordVariable: 'PASSWORD']]) {
       sh """  
-            cd ${repo}
+            cd "${repo}"
             # Retrieve Jenkins CSRF crumb
             CRUMB=$(curl -s 'http://192.168.18.9:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
             curl -XPOST 'http://192.168.18.9:8080/createItem?name=${appName}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22${appName}%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' --user ${USER}:${PASSWORD} -H "${CRUMB}" -H "Content-Type:application/x-www-form-urlencoded"
