46f95ff211af0823edfdb6fadf7d7999c919869e
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-09 23:39:37 +0800

    auto create job prepared

--- src/com/seh/utils/autoCreateJob.groovy
+++ src/com/seh/utils/autoCreateJob.groovy
@@ -21,7 +21,7 @@ def call(String repo,String appName) {
 
             auth_token=”${USER}:${PASSWORD}”
 
-            http_response=\$(curl — cookie-jar ./cookie -s “http://192.168.18.11:8080/jenkins/crumbIssuer/api/json" --user ${auth_token})
+            http_response=\$(curl — cookie-jar ./cookie -s 'http://192.168.18.11:8080/jenkins/crumbIssuer/api/json' --user ${auth_token})
             CRUMB=\$(echo "${http_response}" | jq -r '.crumb')
             echo "${CRUMB}"
             echo "${auth_token}"
