2191efa173d7a18ceb1b2d237ed7963d889ea102
minaxi <minaxijoshi3101@gmail.com>
2024-03-16 17:15:08 +0800

    createViewAndJobsInJenkins.groovy: agent addedd

--- vars/createViewAndJobsInJenkins.groovy
+++ vars/createViewAndJobsInJenkins.groovy
@@ -9,64 +9,73 @@ def call(body) {
     String appName;
     List<String> repoNames = Arrays.asList(params.REPO_NAME)
     try {
-    timeout(time: 60, unit: 'MINUTES') {
-    pipeline {
-        /* agent {
-            label 'devops_automation'
-        } */
-        agent { label 'lxsehin003'}
-        options {
-            skipDefaultCheckout()
-            timestamps()
-            buildDiscarder(logRotator(numToKeepStr: '20'))
-        }
-        environment{
-            BUILDSERVER_WORKSPACE = "${WORKSPACE}"
-            APPLICATION_CODE = "${params.APP_CODE}"
-        }
-        stages {
-            stage("Create view in Jenkins") {
-                steps {
-                    script{
-                        Jenkins jenkins = Jenkins.getInstance()
-                        def viewName = "${APPLICATION_CODE}"
-                        println "view name is "+viewName
-                        def myView = hudson.model.Hudson.instance.getView(viewName)
-                        if (myView == null) {
-                        jenkins.addView(new ListView(viewName))
-                        myView.save()
-                        // get the view
-                        }
-                        println "view is "+myView
-                        println "view created "+myView
+        timeout(time: 60, unit: 'MINUTES') {
+            pipeline {
+                /* agent {
+                    label 'devops_automation'
+                } */
+                agent { label 'lxsehin003' }
+                options {
+                    skipDefaultCheckout()
+                    timestamps()
+                    buildDiscarder(logRotator(numToKeepStr: '20'))
+                }
+                environment{
+                    BUILDSERVER_WORKSPACE = "${WORKSPACE}"
+                    APPLICATION_CODE = "${params.APP_CODE}"
+                }
+                stages {
+                    stage("Create view in Jenkins") {
+                        steps {
+                            script{
+                                Jenkins jenkins = Jenkins.getInstance()
+                                def viewName = "${APPLICATION_CODE}"
+                                println "view name is "+viewName
+                                def myView = hudson.model.Hudson.instance.getView(viewName)
+                                if (myView == null) {
+                                jenkins.addView(new ListView(viewName))
+                                myView.save()
+                                // get the view
+                                }
+                                println "view is "+myView
+                                println "view created "+myView
+                            }
+                        }   
                     }
-                }   
-            }
-            stage("Create Jenkins Job") {
-                steps{
-                    script{
-                        for(repo in repoNames) {
-                            new autoCreateJob().call(repo,APPLICATION_CODE)
+                    stage("Create Jenkins Job") {
+                        steps{
+                            script{
+                                for(repo in repoNames) {
+                                    new autoCreateJob().call(repo,APPLICATION_CODE)
+                                }
+                            }
+                        }
+                    }
+                    /* stage("Create Repos") {
+                        steps {
+                            script {
+                                for(repo in repoNames) {
+                                    new autoCreateRepos().call(repo,appName)
+                                }
+                            }
+                        }   
+                    } */
+                }
+                post {
+                    always {
+                        script {
+                            if (getContext(hudson.FilePath)) {
+                                deleteDir()
+                            }
                         }
                     }
                 }
             }
-            /* stage("Create Repos") {
-                steps {
-                    script {
-                        for(repo in repoNames) {
-                            new autoCreateRepos().call(repo,appName)
-                        }
-                    }
-                }   
-            } */
         }
-        }
+    }  catch (err) {
+        echo "Caught: ${err}"
+        throw err
+    }finally {
+        cleanWs()
     }
-    }   catch (err) {
-            echo "Caught: ${err}"
-            throw err
-        }finally {
-            cleanWs()
-        }
 }
