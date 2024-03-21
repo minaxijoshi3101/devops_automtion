367e7aebbff204143672cf14167086d102b34353
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-21 16:06:37 +0800

    autoCreateJob.groovy: syntax correction

--- src/com/seh/utils/autoCreateJob.groovy
+++ src/com/seh/utils/autoCreateJob.groovy
@@ -22,10 +22,7 @@ def call(String repo,String appName) {
                         #create sonar scan common job for all the modules
 
                         #Create folder as per repo name
-                        curl -s -XPOST 'http://192.168.18.6:8080/view/SEH/createItem?name=${repo}&mode=com.cloudbees.hudson.plugins.folder.#Folder&from=&json=%7B%22name%22%3A%22SEH%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
-                        -u "${USER}:1118a269715e91e16144ecae875f87060f" \
-                        -H "Content-Type:application/x-www-form-urlencoded" \
-                        -H "\$CRUMB"
+                        curl -s -XPOST 'http://192.168.18.6:8080/view/SEH/createItem?name=${repo}&mode=com.cloudbees.hudson.plugins.folder.#Folder&from=&json=%7B%22name%22%3A%22SEH%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' -u "${USER}:1118a269715e91e16144ecae875f87060f" -H "Content-Type:application/x-www-form-urlencoded" -H "\$CRUMB"
 		
                         #Create jenkins job
                         #Create build and depolyment job
