af6a196a720a06f2a96ef2fc19208b8616448780
minaxi <minaxijoshi3101@gmail.com>
2024-03-21 20:37:12 +0800

    autoCreateGitHubRepo.groovy: curl with github api to create repos

--- src/com/seh/utils/autoCreateGitHubRepo.groovy
+++ src/com/seh/utils/autoCreateGitHubRepo.groovy
@@ -18,6 +18,17 @@ def call(String repo,String appName) {
     """
     withCredentials([string(credentialsId: 'GITHUB_TOKEN', variable: 'githubToken')]) {
         sh """
+            // Execute cURL command to create the repository
+            def command = "curl -X POST -H 'Authorization: token ${githubToken}' -d '${repoConfigJson}' ${githubUrl}"
+            def process = command.execute()
+            process.waitFor()
+            // Check if the repository creation was successful
+            if (process.exitValue() == 0) {
+                println "Repository ${repoName} created successfully on GitHub."
+            } else {
+                println "Failed to create repository ${repoName}."
+                println "Error: ${process.err.text}"
+            }
         """
     }
     try{
