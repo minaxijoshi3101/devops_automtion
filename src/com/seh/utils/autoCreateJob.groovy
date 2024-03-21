aaa9c08431acf1d283c4971dfe1b8d0e86fc913c
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-21 16:03:59 +0800

    autoCreateJob.groovy: syntax correction

--- src/com/seh/utils/autoCreateJob.groovy
+++ src/com/seh/utils/autoCreateJob.groovy
@@ -21,38 +21,11 @@ def call(String repo,String appName) {
                        
                         #create sonar scan common job for all the modules
 
-                        curl -XPOST 'http://192.168.18.6:8080/view/SEH/createItem?name=sonar_scan' \
-                        --data-binary @sonar_scan.xml \
-                        -H "Content-Type:text/xml" \
-                        -u "${USER}:1118a269715e91e16144ecae875f87060f" \
-                        -H \$CRUMB
-
                         #Create folder as per repo name
                         curl -s -XPOST 'http://192.168.18.6:8080/view/SEH/createItem?name=${repo}&mode=com.cloudbees.hudson.plugins.folder.#Folder&from=&json=%7B%22name%22%3A%22SEH%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
                         -u "${USER}:1118a269715e91e16144ecae875f87060f" \
                         -H "Content-Type:application/x-www-form-urlencoded" \
                         -H "\$CRUMB"
-                        
-                        #create folders for SIT, UAT, PRE-PROD and PROD
-                        curl -XPOST 'http://10.13.12.106:8080/view/SEH/job/${repo}/createItem?name=SIT&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22SIT%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
-                        --user ${USER}:1118a269715e91e16144ecae875f87060f \
-                        -H "Content-Type:application/x-www-form-urlencoded" \
-                        -H "\$CRUMB
-
-                        curl -XPOST 'http://10.13.12.106:8080/view/SEH/job/${repo}/createItem?name=UAT&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22SIT%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
-                        --user ${USER}:1118a269715e91e16144ecae875f87060f \
-                        -H "Content-Type:application/x-www-form-urlencoded" \
-                        -H "\$CRUMB
-
-                        curl -XPOST 'http://10.13.12.106:8080/view/SEH/job/${repo}/createItem?name=PRE_PROD&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22SIT%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
-                        --user ${USER}:1118a269715e91e16144ecae875f87060f \
-                        -H "Content-Type:application/x-www-form-urlencoded" \
-                        -H "\$CRUMB
-
-                        curl -XPOST 'http://10.13.12.106:8080/view/SEH/job/${repo}/createItem?name=PROD&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22SIT%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' \
-                        --user ${USER}:1118a269715e91e16144ecae875f87060f \
-                        -H "Content-Type:application/x-www-form-urlencoded" \
-                        -H "\$CRUMB
 		
                         #Create jenkins job
                         #Create build and depolyment job
