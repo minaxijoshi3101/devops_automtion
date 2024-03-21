1b39b1c5b58b85bbb3abcc58f373a30de2182fd1
minaxi <minaxijoshi3101@gmail.com>
2024-03-21 20:35:15 +0800

    autoCreateGitHubRepo.groovy defcall added

--- src/com/seh/utils/autoCreateGitHubRepo.groovy
+++ src/com/seh/utils/autoCreateGitHubRepo.groovy
@@ -1,1 +1,19 @@
 package com.seh.utils;
+
+package com.seh.utils;
+
+def call(String repo,String appName) {
+    def githubUrl = 'https://api.github.com/user/repos'
+    def owner = 'minaxijoshi3101'
+    def repoName = "${repo}"
+
+    // Define repository configuration JSON payload
+    def repoConfigJson = """
+    {
+    "name": "${repoName}",
+    "private": false,
+    "auto_init": true,
+    "gitignore_template": "nanoc"
+    }
+    """
+}
