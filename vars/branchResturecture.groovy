56f02206dc3ccc4d8fe58154b5b99b8d739513f8
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-24 01:15:38 +0800

    branchResturecture.groovy: created

--- vars/branchResturecture.groovy
+++ vars/branchResturecture.groovy
@@ -1,48 +1,46 @@
-//def call(body) {
-    pipeline {
-        options {
-            skipDefaultCheckout()
-        }
-        /* agent {
-            label 'devops_automation'
-        } */
-        agent any
-        stages {
-            stage('Git Operations') {
-                steps {
-                    script {
-                        cleanWs()
-                        def random = new Random()
-                        def randomNumber = random.nextInt(1000)
-                        def fileName = "test_${randomNumber}.txt"
-                        withCredentials([usernamePassword(credentialsId: 'devops_automation_buildUser', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-                            sh """
-                                git clone "https://${GIT_USERNAME}:Joshi%402405@github.com/minaxijoshi3101/seh-students.git"
-                            """
-                            dir("seh-students"){
-                                // Checkout release/t3 branch
-                                sh 'git checkout release/release_t3'
-                                sh 'git config http:sslVerify false'
-                                // Create and checkout new branch
-                                sh 'git checkout -b release/release_t3_1.2.3.4'
-                                // Check if dbscripts folder exists
-                                def dbscriptsExists = fileExists('dbscripts')
-                                if (dbscriptsExists) {
-                                    // Remove files from dbscripts folder
-                                    sh "rm -rf dbscripts/*"
-                                    sh 'touch $fileName'
-                                } else {
-                                    echo 'dbscripts folder does not exist'
-                                }
-                                
-                                // Push new branch
-                                sh 'git push origin release/t3_1.2.3.4'
+pipeline {
+    options {
+        skipDefaultCheckout()
+    }
+    /* agent {
+        label 'devops_automation'
+    } */
+    agent any
+    stages {
+        stage('Git Operations') {
+            steps {
+                script {
+                    cleanWs()
+                    def random = new Random()
+                    def randomNumber = random.nextInt(1000)
+                    def fileName = "test_${randomNumber}.txt"
+                    withCredentials([usernamePassword(credentialsId: 'devops_automation_buildUser', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
+                        sh """
+                            git clone "https://${GIT_USERNAME}:Joshi%402405@github.com/minaxijoshi3101/seh-students.git"
+                        """
+                        dir("seh-students"){
+                            // Checkout release/t3 branch
+                            sh 'git checkout release/release_t3'
+                            //sh 'git config http:sslVerify false'
+                            // Create and checkout new branch
+                            sh 'git checkout -b release/release_t3_1.2.3.4'
+                            // Check if dbscripts folder exists
+                            def dbscriptsExists = fileExists('cbo/dbscripts')
+                            if (dbscriptsExists) {
+                                // Remove files from dbscripts folder
+                                sh "rm -rf dbscripts/*"
+                                sh 'touch $fileName'
+                            } else {
+                                echo 'dbscripts folder does not exist'
                             }
-                        
+                            
+                            // Push new branch
+                            sh 'git push origin release/t3_1.2.3.4'
                         }
+                    
                     }
                 }
             }
         }
     }
-//}
+}
