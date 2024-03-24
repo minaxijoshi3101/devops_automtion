c15e60d365330a8cd33c34e12a395fa212f42f6a
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-03-24 22:28:17 +0800

    git command updated

--- src/com/seh/utils/autoCreateRepo.groovy
+++ src/com/seh/utils/autoCreateRepo.groovy
@@ -9,17 +9,18 @@ def call(String repo,String appName) {
     def repoConfigJson = """
     {
     "name": "${repoName}",
+    "description":"This is your first repo!",
+    "homepage":"https://github.com",
     "private": false,
-    "auto_init": true,
-    "gitignore_template": "nanoc"
+    "is_template":true
     }
     """
     // Define repository configuration JSON payload
     //def repoConfigJson 
-    withCredentials([string(credentialsId: 'GITHUB_TOKEN', variable: 'githubToken')]) {
+    withCredentials([string(credentialsId: 'GITHUB_TOKEN', variable: 'GITHUB_TOKEN')]) {
         sh """
             #Execute cURL command to create the repository
-            command="curl -X POST -H Authorization: token ${githubToken} -d ${repoConfigJson} ${githubUrl}"
+            command="curl -L -X POST -H Accept: application/vnd.github+json -H Authorization: Bearer ${GITHUB_TOKEN} -H "X-GitHub-Api-Version: 2022-11-28" -d ${repoConfigJson} ${githubUrl}"
             echo "Executing: \${command}"
             \${command}
         """
